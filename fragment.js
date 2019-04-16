53c78f238724a7c5fef2f64fff630e640b743a83
Daniels Kursits <daniels.kursits@gmail.com>
2019-04-16 23:13:45 +0300

    Add after-render logic

--- fragment.js
+++ fragment.js
@@ -12,16 +12,35 @@ class Fragment {
         this.parent = parent;
 
         /**
+         * Define getters/setters to rerender when data changes
+         */
+        this.__observed = {};
+
+        /**
+         * First render lock
+         * Fragment must be explicitly rendered by f.render() to enable watchers
+         */
+        this.__render_lock = true;
+
+        /**
+         * Queue to call after rendering nested fragments
+         */
+        this.__after_render_queue = [];
+
+        /**
          * Prepare data, all fields set in this constructor will be captured
          */
         this.watch(...args);
 
-        /**
-         * Define getters/setters to rerender when data changes
-         */
-        this.__observed = {};
         for (let key in this) {
-            if (key != "__observed" && key != "parent") {
+            if (
+                [
+                    "parent",
+                    "__observed",
+                    "__render_lock",
+                    "__after_render_queue",
+                ].indexOf(key) < 0
+            ) {
                 /**
                  * Redirect lookup and save
                  */
@@ -35,7 +54,7 @@ class Fragment {
                         get: (t, k) => t[k],
                         set: (t, k, v) => {
                             t[k] = v;
-                            this.render();
+                            if (!this.__render_lock) this.render();
                             return true;
                         },
                     });
@@ -48,7 +67,7 @@ class Fragment {
                     get: () => this.__observed[key],
                     set: value => {
                         this.__observed[key] = value;
-                        this.render();
+                        if (!this.__render_lock) this.render();
                         return true;
                     },
                 });
@@ -59,14 +78,13 @@ class Fragment {
          * Normal initialization
          */
         this.init(...args);
-        this.fragment = null;
 
         /**
          * Since constructor returns rendering proxy,
          * provide access to both the proxy instance and real instance
          */
         this._real = this;
-        this._proxy = new Proxy(() => this._call(), {
+        this._proxy = new Proxy(() => this._render(), {
             get: (t, k) => this[k],
             set: (t, k, v) => {
                 this[k] = v;
@@ -80,23 +98,35 @@ class Fragment {
     /**
      * Render-on-call handler, don't override unless you want to do something extra at render time
      */
-    _call() {
+    _render() {
+        this.queueAfterRender(this);
         return this.template;
     }
+
+    queueAfterRender(element) {
+        if (this.parent instanceof Node) {
+            this.__after_render_queue.push(element);
+        } else {
+            this.parent.queueAfterRender(element);
+        }
+    }
+
+    afterRender() {}
 
     /**
      * Render handler
      */
     render() {
+        this.__render_lock = false;
+
         if (this.parent instanceof Node) {
-            render(this.parent, () => this._call());
+            render(this.parent, () => this._render());
+            this.__after_render_queue.forEach(element => element.afterRender());
+            this.afterRender();
+            this.__after_render_queue = [];
         } else {
             this.parent.render();
         }
-    }
-
-    afterRender(el) {
-        return el;
     }
 
     /**
@@ -113,7 +143,7 @@ class Fragment {
      * Should return: html`...`
      * @return {Node|object}
      */
-    template() {}
+    get template() {}
 
     /**
      * Plug method
